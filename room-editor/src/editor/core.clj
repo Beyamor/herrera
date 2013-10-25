7f0e0b0b2d5505247894ee40116bf74a9c421010
beyamor <beyamor@gmail.com>
2013-10-25 11:38:41 -0700

    Selecting a file to load

--- room-editor/src/editor/core.clj
+++ room-editor/src/editor/core.clj
@@ -1,10 +1,35 @@
 (ns editor.core
-  (:use seesaw.core))
+  (:use seesaw.core)
+  (:import java.awt.FileDialog))
+
+(defn new-model
+  []
+  (atom {}))
+
+(defn choose-file
+  [frame]
+  (->
+    (doto (FileDialog. frame "Choose a file", FileDialog/LOAD)
+      (.setDirectory "../app/coffee/game")
+      (.setVisible true))
+    .getFile))
 
 (defn -main [& args]
+  (let [model (new-model)
+        root (frame :title "Room Editor")
+        load-action (action
+                      :handler (fn [e]
+                                 (when-let [file-name (choose-file root)]
+                                   (config! (select root [:#content]) :text file-name)))
+                      :name "Load..."
+                      :key "menu L")]
   (invoke-later
-    (-> (frame :title "Room Editor"
-               :content "Hi!"
-               :on-close :exit)
+    (-> root
+      (config! :size [600 :by 600])
+      (config! :content
+               (border-panel
+                 :north (toolbar :items [load-action])
+                 :center (label :id :content
+                                :text "Hello")))
       pack!
-      show!)))
+      show!))))
